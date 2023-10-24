fa0bc1758bd7e21c26dedcb82feecadc075e5189
Rohit Agrawal <rohit777agrawal@gmail.com>
2023-10-24 16:04:24 -0700

    JavaScript Projects

---
+++ index.js
@@ -0,0 +1,99 @@
+
+
+
+
+
+
+//Creating a number guessing game
+// const answer = Math.floor(Math.random() *10 + 1);
+// let guesses = 0;
+
+// document.getElementById("submitButton").onclick = function(){
+//     let guess = document.getElementById("guessField").value;
+//     guesses+=1;
+
+//     if(guess == answer){
+//         alert(`${answer} is the number. It took you ${guesses} to guess`);
+//     }
+//     else if(guess < answer){
+//         alert("Too small");
+//     }
+//     else{
+//         alert("Too large")
+//     }
+
+
+// }
+
+
+//Template Literals = use delimeter(`) helps in strings and sentences ${userName}
+// condition ? expreIfTrue : expreIfFalse
+
+//Important JavaScript: Create a checklist and radio button
+// const subscribe = document.getElementById("myCheckbox");
+// const visa = document.getElementById("visa");
+// const mastercard= document.getElementById("mastercard");
+// const paypal = document.getElementById("paypal");
+
+// document.getElementById("myButton").onclick = function(){
+//     if(subscribe.checked == true){
+//         document.getElementById("mySubscribe").innerHTML = "You are Subscribed";
+//     }
+//     else{
+//         document.getElementById("mySubscribe").innerHTML = "You are not Subscribed";
+//     }
+
+//     if(visa.checked){
+//         console.log("You selected visa payment method");
+//     }
+//     else if(mastercard.checked){
+//         console.log("You selected mastercard payment method");
+//     }
+//     else if(paypal.checked){
+//         console.log("You selected paypal payment method");
+//     }
+//     else{
+//         console.log("You need to select one!");
+//     }
+// }
+
+
+
+
+
+
+//Creating a counter program
+// let count = 0;
+
+// document.getElementById("decrease").onclick = function(){
+//     count -= 1;
+//     document.getElementById("countLabel").innerHTML = count;
+// }
+
+// document.getElementById("reset").onclick = function(){
+//     count = 0;
+//     document.getElementById("countLabel").innerHTML = count;
+// }
+
+// document.getElementById("increase").onclick = function(){
+//     count += 1;
+//     document.getElementById("countLabel").innerHTML = count;
+// }
+
+
+
+
+
+// console.log("Done");
+
+// let firstname = "bro";
+// document.getElementById("p3") = "Hello" + firstname;
+
+//Helps to prompt for data
+// let username = window.prompt("What's your name?");
+
+//Helps to take a input from the html button and save the data.  COOOOL
+// document.getElementById("myButton").onclick = function(){
+//     username = document.getElementById("myText").value;
+//     console.log(username);
+// }
